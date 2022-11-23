91b3f2141dc8d2556bcb97be9440cd3142e73361
steve1337 <22643375+steve1337@users.noreply.github.com>
2022-11-23 20:05:19 +0800

    add option to pass JVM args

--- karate.js
+++ karate.js
@@ -6,11 +6,14 @@ karate.executable = function () {
 	let prefix = karate.config.dir ? '-Dkarate.config.dir=' + karate.config.dir + ' '  : '';
 	return prefix + 'com.intuit.karate:karate-core:' + karate.version + ':all';
 };
-karate.exec = function (args) {
+karate.exec = function (args, jvmArgs) {
 	if (!args) {
 		args = process.argv.slice(2).join(' ');
 	}
+	if(!jvmArgs){
+		jvmArgs = ""
+	}
 	process.env['KARATE_META'] = 'npm:' + process.env.npm_package_version;
-	jbang.exec(karate.executable() + ' ' + args);
+	jbang.exec(jvmArgs + ' ' + karate.executable() + ' ' + args);
 };
 module.exports = karate;
